a8206d24dbad12476bca19e9322d23da18d0dc9c
eman <emanhharki@gmail.com>
2021-05-06 22:30:44 +0300

    done

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ async function fetchRndomWords()
    let data=await respose.json()
    console.log(data);
 }
+fetchRndomWords();
 
 // buttons for all letters
 const allLetters= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -57,18 +58,6 @@ function guessTheWordLetters()
    console.log(word);
    console.log(correctWord);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
