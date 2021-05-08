36b742b6dca4a33ce6042c7876c241a5457a79cd
dlzheen <dlzheenmahmood@gmail.com>
2021-05-08 11:55:08 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,10 @@ const gameOverrOrLiveNumburs =document.getElementById("game-overr-or-live-numbur
 const clue=document.getElementById("clue");
 const body = document.querySelector("body");
 let hangMan=document.getElementById("hanged-man-picture");
-
+let livesh1 = document.querySelector("#game-overr-or-live-numburs");
 //variables:
 let  guessArray=[];
 let word;
-
-
-
-
 window.onload = function start()
 { 
 async function fetchRndomWords()
@@ -23,29 +19,20 @@ async function fetchRndomWords()
    word = data[0];
    console.log(word[0].length);
    guessTheWordLetters();
-
-// buttons for all letters
+   // buttons for all letters
    const allLetters= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-   
    let counter = 0;
    let numberOfLives = 10;
-   let numsToWin = 0;
    allLetters.forEach(letter=>{
      const button = document.createElement("button");
     button.innerHTML = letter;
     const lettersDiv =  document.getElementById("letters");
     lettersDiv.appendChild(button); 
     button.addEventListener("click", btnClick)
+    
     function btnClick(){
-      let livesh1 = document.querySelector("#game-overr-or-live-numburs");
-      
       if(numberOfLives >1){
-        
-        
-
-        
-        
-      button.style.opacity="50%";
+        button.style.opacity="50%";
       if(word.includes(button.innerText))
       {
        let arrOfIndices = [];
@@ -59,22 +46,15 @@ async function fetchRndomWords()
        console.log(arrOfIndices);
        for(let i=0;i<=arrOfIndices.length;i++){
         guessArray[arrOfIndices[i]]=button.innerText;
-       
-        
-        
-
         console.log(guessArray);
-        
-        let li = document.querySelectorAll("#letters-to-guess li")[arrOfIndices[i]];
+          let li = document.querySelectorAll("#letters-to-guess li")[arrOfIndices[i]];
         li.innerText = guessArray[arrOfIndices[i]];
        }
        button.disabled = true;
-    numsToWin++;
-       
-      }
+       numsToWin++;
+       }
       else 
       {
-        
          let hangMann =document.querySelectorAll(".hangman-figure")[counter];
          hangMann.style.display = "block";
          counter++;
@@ -83,9 +63,7 @@ async function fetchRndomWords()
         livesh1.innerHTML = `you have ${numberOfLives} lives`;
         button.disabled = true;
       }
-        
-      
-    }
+      }
     else{
       livesh1.innerHTML = `your have no lives`;
     console.log("Game over");  
@@ -95,9 +73,13 @@ async function fetchRndomWords()
 
     
 })//for 26
+
 }
 fetchRndomWords();
-
+let showAnswer = document.querySelector("#show-answer");
+showAnswer.addEventListener("click" , function(){
+  livesh1.innerHTML = word;
+});
 //place for the word we should guess 
 function guessTheWordLetters()
 {
@@ -119,8 +101,6 @@ function guessTheWordLetters()
   //  console.log(guessArray);
 }
 }
-
-
 //relod the page to paly again
 document.getElementById("play").addEventListener("click",function reloadThePage()
 {
@@ -129,5 +109,5 @@ document.getElementById("play").addEventListener("click",function reloadThePage(
 
 
 
-//hangman
+
 
